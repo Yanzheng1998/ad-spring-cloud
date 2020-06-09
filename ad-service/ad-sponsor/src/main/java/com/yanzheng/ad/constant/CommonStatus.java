0bda15852f708ea6c302614f1ab911bd13305dba
Yanzheng Li <you@example.com>
2020-06-09 16:27:16 -0500

    add limitation on ad unit

---
+++ ad-service/ad-sponsor/src/main/java/com/yanzheng/ad/constant/CommonStatus.java
@@ -0,0 +1,18 @@
+package com.yanzheng.ad.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum CommonStatus {
+
+    VALID(1, "valid"),
+    INVALID(0, "invalid");
+
+    private Integer status;
+    private String desc;
+
+    CommonStatus(Integer status, String desc) {
+        this.status = status;
+        this.desc = desc;
+    }
+}
